f750e2dea1ebae401292f60daa176b7a599996c6
isabelmelgui <143104109+isabelmelgui@users.noreply.github.com>
2023-08-24 09:54:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ import streamlit as st
 from PIL import image
 
 st.title("La aplicacion de Isabel")
+st.header("En este espacio empiezo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo desarrollar backend y frontend")
+Image = Image.open
