b6382167e5ed292b31d8c3a571cfef71a41be729
isabelmelgui <143104109+isabelmelgui@users.noreply.github.com>
2023-08-24 09:59:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import streamlit as st
 from PIL import image
 
 st.title("La aplicacion de Isabel")
+
 st.header("En este espacio empiezo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write("Facilmente puedo desarrollar backend y frontend")
-Image = Image.open
+image = Image.open('plant.png')
+
+st.image(image, caption= 'Interfaces multimodales')
+
+
+texto = st.text_input('shakira shakira', 'este es mi texto')
+st.write('El texto escrito es', texto)
